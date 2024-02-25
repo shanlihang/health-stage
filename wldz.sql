c0f8dc4fd7bd555fbce0e0310ba272ddf043fe43
slh_home <slh67490009@gmail.com>
2024-02-25 18:01:18 +0800

    数据库文件

---
+++ wldz.sql
@@ -0,0 +1,91 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : mysql
+ Source Server Type    : MySQL
+ Source Server Version : 80031
+ Source Host           : localhost:3306
+ Source Schema         : wldz
+
+ Target Server Type    : MySQL
+ Target Server Version : 80031
+ File Encoding         : 65001
+
+ Date: 25/02/2024 18:00:53
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for goods
+-- ----------------------------
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods`  (
+  `id` bigint(0) NOT NULL COMMENT '物品id',
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '物品名称',
+  `nums` int(0) NULL DEFAULT NULL COMMENT '物品数量',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of goods
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for menu_item
+-- ----------------------------
+DROP TABLE IF EXISTS `menu_item`;
+CREATE TABLE `menu_item`  (
+  `id` bigint(0) NOT NULL COMMENT '菜单项id',
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单项名称',
+  `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单项图标',
+  `type_id` bigint(0) NULL DEFAULT NULL COMMENT '关联菜单类别表id',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of menu_item
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for top_menu
+-- ----------------------------
+DROP TABLE IF EXISTS `top_menu`;
+CREATE TABLE `top_menu`  (
+  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单名称',
+  `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单图标',
+  `router` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单路由',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of top_menu
+-- ----------------------------
+INSERT INTO `top_menu` VALUES (1, '首页', '/src/assets/pic/menus/index.svg', '/');
+INSERT INTO `top_menu` VALUES (2, '医疗管理', '/src/assets/pic/menus/list.svg', '/medical');
+INSERT INTO `top_menu` VALUES (3, '库存管理', '/src/assets/pic/menus/site.svg', '/inventory');
+INSERT INTO `top_menu` VALUES (4, '系统设置', '/src/assets/pic/menus/settings.svg', '/setting');
+INSERT INTO `top_menu` VALUES (5, '数据大屏', '/src/assets/pic/menus/data.svg', '/data');
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `id` bigint(0) NOT NULL COMMENT '用户id',
+  `nickName` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户姓名',
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户账号',
+  `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户密码',
+  `sex` int(0) NULL DEFAULT NULL COMMENT '用户性别（0为男，1为女）',
+  `phone` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户电话',
+  `mail` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '用户邮箱',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+
+SET FOREIGN_KEY_CHECKS = 1;
