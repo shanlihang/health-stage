e7cbc386879931c49525f70eba0360c55c54fa40
shanlihang <slh_ontuo@126.com>
2024-02-26 17:15:57 +0800

    用户中心布局的实现

---
+++ web/src/components/account/AccountManage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="container">
+        11111
+    </div>
+</template>
+
+<style scoped></style>
--- web/src/views/AccountCenter.vue
+++ web/src/views/AccountCenter.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import {ref,onMounted} from 'vue'
+import AccountHome from '/src/components/account/AccountHome.vue'
+import AccountManage from '/src/components/account/AccountManage.vue'
 
 //当前激活的选项卡
 const activeKey = ref<number>(1)
@@ -16,7 +18,8 @@ onMounted(() => {
       tab-position="left"
       :style="{ height: '100%' }"
     >
-      <a-tab-pane v-for="i in 30" :key="i" :tab="`Tab-${i}`">Content of tab {{ i }}</a-tab-pane>
+      <a-tab-pane :key="1" tab="个人中心"><AccountHome /></a-tab-pane>
+      <a-tab-pane :key="2" tab="用户管理"><AccountManage /></a-tab-pane>
     </a-tabs>
   </div>
 </template>
@@ -25,6 +28,7 @@ onMounted(() => {
 .container{
   width: 100%;
   height: 100%;
+  border-radius: 9px;
   background-color: #fff;
 }
 </style>
