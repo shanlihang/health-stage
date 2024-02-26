9eab9dc34fb182939db0e9e8dd312c1353630bf3
shanlihang <slh_ontuo@126.com>
2024-02-26 16:58:59 +0800

    库存管理界面的大致实现

---
+++ web/src/components/inventory/StockManage.vue
@@ -0,0 +1,226 @@
+<script setup lang="ts">
+import {ref,reactive} from 'vue'
+
+
+//搜索表单接口定义
+interface SearchForm{
+    name:string,
+    userId:number,
+    currentPage:number,
+    pageSize:number
+}
+
+//搜索框表单
+const searchForm = reactive<SearchForm>({
+    name:'',
+    userId:1,
+    currentPage:1,
+    pageSize:20
+})
+
+//入库数量
+const insertNum = ref<number>()
+
+//出库数量
+const outsertNum = ref<number>()
+
+//入库弹窗显示状态
+const openInsertGoods = ref<boolean>(false)
+
+const openOutsertGoods = ref<boolean>(false)
+
+//弹窗是否居中显示
+const centered = ref<boolean>(true)
+
+//表格边框显示状态
+const bordered = ref<boolean>(true)
+
+//是否显示表格自带的分页
+const table_page = ref<boolean>(false)
+
+//分页每页显示数量的选项
+const pageSizeOptions = ref<Array<string>>(['10','20','50','100'])
+
+//操作人下拉列表
+const personOptions = reactive([
+    {label:'nick',value:1},
+    {label:'tom',value:2},
+    {label:'lily',value:3},
+    {label:'lim',value:4},
+])
+
+//表格数据
+const data = [
+  {
+    id: 1,
+    name: 'John Brown',
+    num: 32,
+    unit:'个',
+    remark: 'New York No. 1 Lake Park',
+  },
+  {
+    id: 2,
+    name: 'John Brown',
+    num: 32,
+    unit:'个',
+    remark: 'New York No. 1 Lake Park',
+  },
+  {
+    id: 3,
+    name: 'John Brown',
+    num: 32,
+    unit:'个',
+    remark: 'New York No. 1 Lake Park',
+  },
+  {
+    id: 4,
+    name: 'John Brown',
+    num: 32,
+    unit:'个',
+    remark: 'New York No. 1 Lake Park',
+  },
+  {
+    id: 5,
+    name: 'John Brown',
+    num: 32,
+    unit:'个',
+    remark: 'New York No. 1 Lake Park',
+  }
+];
+
+//insert入库事件
+const insert = (record) => {
+    openInsertGoods.value = true
+}
+
+//outsert出库事件
+const outsert = (record) => {
+    openOutsertGoods.value = true
+}
+
+//搜索框表单提交成功
+const handleFinish = () => {}
+
+//搜索框表单提交失败
+const handleFinishFailed = () => {}
+
+//分页数据改变时触发
+const onShowSizeChange = () => {}
+
+//入库弹窗确认
+const insertGoodsOk = () => {
+    openInsertGoods.value = false
+}
+
+//入库弹窗取消
+const insertGoodsCancel = () => {
+    openInsertGoods.value = false
+}
+
+//出库弹窗确认
+const outsertGoodsOk = () => {
+    openOutsertGoods.value = false
+}
+
+//出库弹窗取消
+const outsertGoodsCancel = () => {
+    openOutsertGoods.value = false
+}
+</script>
+
+<template>
+    <div class="section">
+        <div class="form">
+            <a-form layout="inline" :model="searchForm" @finish="handleFinish" @finishFailed="handleFinishFailed">
+                <a-form-item label="物品名称">
+                    <a-input v-model:value="searchForm.name" placeholder="请输入物品名称" />
+                </a-form-item>
+                <a-form-item label="操作人">
+                    <a-select
+                        v-model:value="searchForm.userId"
+                        :options="personOptions"
+                    ></a-select>
+                </a-form-item>
+                <a-form-item>
+                    <a-button type="primary">搜索</a-button>
+                    <a-button style="margin-left: 15px;">重置</a-button>
+                </a-form-item>
+            </a-form>
+        </div>
+        <div class="table">
+            <a-table :data-source="data" :bordered="bordered" :pagination="table_page" size="small">
+                <a-table-column align="center" title="序号" data-index="id" />
+                <a-table-column align="center" title="物品名称" data-index="name" />
+                <a-table-column align="center" title="物品余量">
+                    <template #default="{record}">
+                        {{ record.num }} ({{ record.unit }}) 
+                    </template>
+                </a-table-column>
+                <a-table-column align="center" title="物品备注" data-index="remark" />
+                <a-table-column align="center" title="操作">
+                    <template #default="{record}">
+                        <a-button type="link" primary @click="insert(record)">入库</a-button>
+                        <a-button type="link" danger @click="outsert(record)">出库</a-button>
+                        
+                    </template>
+                </a-table-column>
+            </a-table>
+        </div>
+        <div class="page">
+            <a-pagination
+                v-model:current="searchForm.currentPage"
+                v-model:pageSize="searchForm.pageSize"
+                :pageSizeOptions="pageSizeOptions"
+                show-size-changer
+                :total="500"
+                @showSizeChange="onShowSizeChange"
+            />
+        </div>
+
+        <a-modal v-model:open="openInsertGoods" title="入库" @ok="insertGoodsOk" @cancel="insertGoodsCancel" :centered="centered" cancelText="取消" okText="确认">
+            <div style="display: flex;align-items: center;">
+                <span>入库数量</span>
+                <a-input-number v-model="insertNum" :min="0" style="margin-left: 15px;width: 50%;" placeholder="请输入入库数量"></a-input-number>
+            </div>
+            
+        </a-modal>
+
+        <a-modal v-model:open="openOutsertGoods" title="出库" @ok="outsertGoodsOk" @cancel="outsertGoodsCancel" :centered="centered" cancelText="取消" okText="确认">
+            <div style="display: flex;align-items: center;">
+                <span>出库数量</span>
+                <a-input-number v-model="outsertNum" :min="0" style="margin-left: 15px;width: 50%;" placeholder="请输入入库数量"></a-input-number>
+            </div>
+            
+        </a-modal>
+    </div>
+</template>
+
+<style scoped>
+.section{
+    width: 100%;
+    height: 100%;
+}
+.form{
+    width: 98%;
+    height: 50px;
+    display: flex;
+    align-items: center;
+}
+.add{
+    width: 98%;
+    height: 50px;
+    display: flex;
+    align-items: center;
+}
+.table{
+    width: 98%;
+    margin-top: 15px;
+}
+.page{
+    width: 98%;
+    height: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: right;
+}
+</style>
--- web/src/views/InventoryManage.vue
+++ web/src/views/InventoryManage.vue
@@ -1,13 +1,7 @@
 <script setup lang="ts">
 import {ref,reactive} from 'vue'
 import GoodsManage from '/src/components/inventory/GoodsManage.vue'
-
-interface Goods{
-    name:string
-}
-const formData = reactive<Goods>({
-    name:''
-})
+import StockManage from '/src/components/inventory/StockManage.vue'
 
 //当前激活选项卡
 const activeKey = ref<number>(1)
@@ -20,32 +14,10 @@ const activeKey = ref<number>(1)
       tab-position="left"
       :style="{ height: '100%'}"
     >
-        <a-tab-pane :key="1" tab="物品操作"><GoodsManage /></a-tab-pane>
-        <a-tab-pane :key="2" tab="物品管理">123</a-tab-pane>
+        <a-tab-pane :key="1" tab="物品管理"><GoodsManage /></a-tab-pane>
+        <a-tab-pane :key="2" tab="库存管理"><StockManage /></a-tab-pane>
         <a-tab-pane :key="3" tab="操作记录">789</a-tab-pane>
     </a-tabs>
-
-
-
-
-
-    <!-- <div class="search">
-        <a-form layout="inline" :model="formData">
-            <a-form-item label="物品名称">
-                <a-input v-model:value="formData.name" placeholder="请输入物品名称" />
-            </a-form-item>
-            <a-form-item>
-                <a-button type="primary">搜索</a-button>
-            </a-form-item>
-        </a-form>
-    </div>
-    <div class="action">
-        <a-button type="primary">Primary</a-button>
-        <a-button type="dashed">Dashed</a-button>
-    </div>
-    <div class="table">
-
-    </div> -->
   </div>
 </template>
 
