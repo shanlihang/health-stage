8d8276ac19f3735633b4d15f9550c7e956587991
slh_home <slh67490009@gmail.com>
2024-02-27 20:01:32 +0800

    git忽略文件的设置

--- server/main.go
+++ server/main.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 	//绑定路由规则，执行的函数
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello World!")
+		c.String(http.StatusOK, "hello")
 	})
 	//监听端口，默认为8080
 	r.Run(":9090")
