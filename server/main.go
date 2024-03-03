a2091109137021ba0ffa3c26480432c295721226
slh_home <slh67490009@gmail.com>
2024-03-03 20:49:48 +0800

    配置跨域中间件

--- server/main.go
+++ server/main.go
@@ -1,17 +1,7 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-	"net/http"
-)
+import "server/router"
 
 func main() {
-	//创建路由
-	r := gin.Default()
-	//绑定路由规则，执行的函数
-	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello")
-	})
-	//监听端口，默认为8080
-	r.Run(":9090")
+	router.LoadRouters()
 }
