06f18e42e5017f4bbc5610d42b1362a8a4437fc8
slh_home <slh67490009@gmail.com>
2024-03-05 21:58:21 +0800

    替换sql文件

--- wldz.sql
+++ wldz.sql
@@ -11,7 +11,7 @@
  Target Server Version : 80031
  File Encoding         : 65001
 
- Date: 25/02/2024 18:00:53
+ Date: 05/03/2024 21:58:06
 */
 
 SET NAMES utf8mb4;
@@ -53,21 +53,24 @@ CREATE TABLE `menu_item`  (
 -- ----------------------------
 DROP TABLE IF EXISTS `top_menu`;
 CREATE TABLE `top_menu`  (
-  `id` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `ID` bigint(0) NOT NULL AUTO_INCREMENT COMMENT '编号',
   `name` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单名称',
   `icon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单图标',
   `router` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '菜单路由',
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  `create_at` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `update_ar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `deleted_at` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of top_menu
 -- ----------------------------
-INSERT INTO `top_menu` VALUES (1, '首页', '/src/assets/pic/menus/index.svg', '/');
-INSERT INTO `top_menu` VALUES (2, '医疗管理', '/src/assets/pic/menus/list.svg', '/medical');
-INSERT INTO `top_menu` VALUES (3, '库存管理', '/src/assets/pic/menus/site.svg', '/inventory');
-INSERT INTO `top_menu` VALUES (4, '系统设置', '/src/assets/pic/menus/settings.svg', '/setting');
-INSERT INTO `top_menu` VALUES (5, '数据大屏', '/src/assets/pic/menus/data.svg', '/data');
+INSERT INTO `top_menu` VALUES (1, '首页', '/src/assets/pic/menus/index.svg', '/', NULL, NULL, NULL);
+INSERT INTO `top_menu` VALUES (2, '医疗管理', '/src/assets/pic/menus/list.svg', '/medical', NULL, NULL, NULL);
+INSERT INTO `top_menu` VALUES (3, '库存管理', '/src/assets/pic/menus/site.svg', '/inventory', NULL, NULL, NULL);
+INSERT INTO `top_menu` VALUES (4, '系统设置', '/src/assets/pic/menus/settings.svg', '/setting', NULL, NULL, NULL);
+INSERT INTO `top_menu` VALUES (5, '数据大屏', '/src/assets/pic/menus/data.svg', '/data', NULL, NULL, NULL);
 
 -- ----------------------------
 -- Table structure for users
